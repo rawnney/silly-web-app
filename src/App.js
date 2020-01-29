5f26614ed3275117d9b55fa6bf30853bcb187f11
Niclas Snellman <nici@bambuser.com>
2020-01-29 10:42:16 +0100

    Add an input field

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  onInputChange = ({target}) => {
+    const { value } = target;
+    this.setState({value});
+  };
+
   render() {
     return (
       <div className="App">
+        <input
+          placeholder='Start typing'
+          value={this.state.value}
+          onChange={this.onInputChange}
+          />
       </div>
     );
   }
